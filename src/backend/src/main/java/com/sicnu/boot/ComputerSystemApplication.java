1df9667b487ec6adfeaff3d39ef7dc3f826909f2
胡建华 <2168289534@qq.com>
2022-09-10 20:18:33 +0800

    后端代码提交

---
+++ src/backend/src/main/java/com/sicnu/boot/ComputerSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sicnu.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComputerSystemApplication.class, args);
+    }
+
+}
