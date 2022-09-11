9317d67b3bf9c272147bd15a38323eaee7707c94
胡建华 <2168289534@qq.com>
2022-09-11 11:06:55 +0800

    使访问接口时，spring-security失效，无需账号和密码

---
+++ src/main/java/com/sicnu/boot/config/CloseSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.sicnu.boot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @projectName: computer-system
+ * @package: com.sicnu.boot.config
+ * @className: CloseSecurityConfig
+ * @author: hjh
+ * @description: 使访问接口时，spring-security失效，无需账号和密码
+ * @date: 2022-09-11 10:12
+ * @version:
+ */
+
+@Configuration
+@EnableWebSecurity
+public class CloseSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //super.configure(http);
+        http.csrf().disable();
+        //配置不需要登陆验证
+        http.authorizeRequests().anyRequest().permitAll().and().logout().permitAll();
+    }
+}
