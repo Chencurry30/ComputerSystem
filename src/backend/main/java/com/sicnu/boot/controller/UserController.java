b3d543bea5832a602cea1118397028ab1bf1d9c2
胡建华 <2168289534@qq.com>
2022-09-17 09:49:24 +0800

    更改获取前台数据方式为json格式

--- src/backend/main/java/com/sicnu/boot/controller/UserController.java
+++ src/backend/main/java/com/sicnu/boot/controller/UserController.java
@@ -28,25 +28,47 @@ public class UserController {
     private ISmsService smsService;
 
     @PostMapping("/login")
-    public ServerResponse<Map<String,String>> login(String username, String password, HttpSession session){
+    public ServerResponse<Map<String,String>> login(@RequestBody Map<String,String> map, HttpSession session){
+        //通过map集合获取json数据
+        if(!map.containsKey("username")){
+            return ServerResponse.createByErrorMessage("用户名为空");
+        }else if (!map.containsKey("password")){
+            return ServerResponse.createByErrorMessage("密码为空");
+        }
+        //取出username和password
+        String username = map.get("username");
+        String password = map.get("password");
         ServerResponse<Map<String,String>> response = userService.login(username, password);
         if(response.isSuccess()){
             //判断是否成功登录，如果成功，存放token
-            Map<String,String> map = new HashMap<>();
-            map.put("token",session.getId());
-            return ServerResponse.createBySuccess("登录成功",map);
+            Map<String,String> res = new HashMap<>();
+            res.put("token",session.getId());
+            return ServerResponse.createBySuccess("登录成功",res);
         }
         log.info("username:{}, password:{}, data:{}", username, password, response.getData());
         return response;
     }
 
     @PostMapping("/register")
-    public ServerResponse<String> register(String username,String password){
+    public ServerResponse<String> register(@RequestBody Map<String,String> map){
+        //通过map集合获取json数据
+        if(!map.containsKey("username")){
+            return ServerResponse.createByErrorMessage("用户名为空");
+        }else if (!map.containsKey("password")){
+            return ServerResponse.createByErrorMessage("密码为空");
+        }
+        //取出username和password
+        String username = map.get("username");
+        String password = map.get("password");
         return userService.register(username, password);
     }
 
     @PostMapping("/sms")
-    public ServerResponse<String> sendSms(String phone){
+    public ServerResponse<String> sendSms(@RequestBody Map<String,String> map){
+        if (!map.containsKey("phone")){
+            return ServerResponse.createByErrorMessage("手机号为空");
+        }
+        String phone = map.get("phone");
         return smsService.sendSmsCode(phone);
     }
 
