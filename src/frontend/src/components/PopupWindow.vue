f2e9311772c05c2187936f312699b72aa3cc6559
李国强 <1473851665@qq.com>
2022-09-23 14:50:29 +0800

    添加注册页面

--- src/frontend/src/components/PopupWindow.vue
+++ src/frontend/src/components/PopupWindow.vue
@@ -25,16 +25,7 @@ name:'PopupWindow',
       };
     },
     methods: {
-      handleClose(done) {
-        this.$confirm('确认关闭？')
-          .then(_ => {
-            done();
-          })
-          .catch(_ => {});
-      },
-      show(){
-        this.dialogVisible = true
-      }
+
     }
   };
 </script>
