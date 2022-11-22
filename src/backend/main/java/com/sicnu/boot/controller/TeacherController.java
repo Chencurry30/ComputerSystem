c5681d63187302b447560a2f7f95f10bb10c4bb5
wangyang <2201675527@qq.com>
2022-11-22 12:19:54 +0800

    修改bug

--- src/backend/main/java/com/sicnu/boot/controller/TeacherController.java
+++ src/backend/main/java/com/sicnu/boot/controller/TeacherController.java
@@ -8,7 +8,6 @@ import com.sicnu.boot.pojo.Teacher;
 import com.sicnu.boot.service.TeacherService;
 import com.sicnu.boot.utils.ServerResponse;
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
