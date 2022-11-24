cf47767f40ae47c3f5eb54907dee5d480fac3878
wangyang <2201675527@qq.com>
2022-11-24 09:58:55 +0800

    修改了一些小问题

--- src/backend/main/java/com/sicnu/boot/mapper/TeacherQuestionMapper.java
+++ src/backend/main/java/com/sicnu/boot/mapper/TeacherQuestionMapper.java
@@ -1,9 +1,11 @@
 package com.sicnu.boot.mapper;
 
 import com.sicnu.boot.pojo.TeacherQuestion;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
+@Mapper
 public interface TeacherQuestionMapper {
     /**
      * 通过id查询对老师的评论
