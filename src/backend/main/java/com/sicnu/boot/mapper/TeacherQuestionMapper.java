dd8baefaa9f4f5f2853bb7c2c350a19693aebfa1
wangyang <2201675527@qq.com>
2022-11-26 20:28:25 +0800

    对老师提问、回复功能

--- src/backend/main/java/com/sicnu/boot/mapper/TeacherQuestionMapper.java
+++ src/backend/main/java/com/sicnu/boot/mapper/TeacherQuestionMapper.java
@@ -8,9 +8,29 @@ import java.util.List;
 @Mapper
 public interface TeacherQuestionMapper {
     /**
-     * 通过id查询对老师的评论
+     * 通过老师id查询对老师的提问
      * @param teacherId：老师id
      * @return TeacherQuestion
      */
      List<TeacherQuestion> getTeacherQuestionById(Integer teacherId);
+
+    /**
+     * 通过老师ID插入提问
+     * @param teacher：老师
+     */
+    void insertTeacherQuestion(TeacherQuestion teacher);
+
+    /**
+     * 通过老师id查询老师未进行回复的提问
+     * @param teacherId：老师id
+     * @return TeacherQuestion
+     */
+    List<TeacherQuestion> getTeacherAnswerById(Integer teacherId);
+
+    /**
+     * 将已回复消息设置为不可见
+     * @param userId：老师
+     */
+    void updateTeacherAnswer(Integer userId);
+
 }
