fed474ed9bf65a3e3ddf99730fea8a3d9a5fbc31
wangyang <2201675527@qq.com>
2022-12-14 09:37:11 +0800

    修改bug

--- src/backend/main/java/com/sicnu/boot/pojo/Message.java
+++ src/backend/main/java/com/sicnu/boot/pojo/Message.java
@@ -1,5 +1,6 @@
 package com.sicnu.boot.pojo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,6 +19,7 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Message {
     private Integer messageId;
     private Integer userId;
